23d6b1b096c01f81126dc8f7e2ad96d91f679ee2
Jonathan Juursema <github@jonathanj.nl>
2022-01-09 20:00:25 +0100

    Right dev API url...

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -11,7 +11,7 @@ const configureStore = (preloadedState: any) => configure({
   preloadedState,
   middleware: [
     authMiddleware,
-    apiMiddleware('https://api.dev.kick-in.media'),
+    apiMiddleware('https://dev.api.kick-in.media'),
     ...(process.env.NODE_ENV === "production" ? [] : [createLogger({
       collapsed: true,
       diff: true,
