3ea145621934e797ff2d43278dfb9422d3030ed7
Jeffrey Bakker <jeffrey.bakker@gmail.com>
2022-01-30 16:57:58 +0100

    define different API environments

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -6,12 +6,15 @@ import reducer from './reducers/reducers';
 import { authMiddleware } from "./middlewares/auth";
 import { apiMiddleware } from "./middlewares/api";
 
+const API_HOST_PROD = 'https://api.kick-in.media';
+const API_HOST_DEV = 'https://api.dev.kick-in.media';
+
 const configureStore = (preloadedState: any) => configure({
   reducer,
   preloadedState,
   middleware: [
     authMiddleware,
-    apiMiddleware('https://api.dev.kick-in.media'),
+    apiMiddleware(process.env.API_ENV === 'prod' ? API_HOST_PROD : API_HOST_DEV),
     ...(process.env.NODE_ENV === "production" ? [] : [createLogger({
       collapsed: true,
       diff: true,
