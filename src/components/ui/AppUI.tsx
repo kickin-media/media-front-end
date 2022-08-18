0d932378da453884c7f92dced986ef6403e78675
Jeffrey Bakker <jeffrey.bakker@gmail.com>
2022-08-18 21:32:42 +0200

    add a disclaimer to the footer

--- src/components/ui/AppUI.tsx
+++ src/components/ui/AppUI.tsx
@@ -180,6 +180,8 @@ const AppUI: React.FC<Props> = ({children}) => {
             </Grid>
             <Grid item xs={12} md={6}>
               <b>Kick-In Media App</b><br />
+              <em>If you want a photo to be removed, please send an e-mail to the <a href="mailto:">Kick-In committee</a>.</em><br />
+              <br />
               <a href="https://www.kick-in.nl">Kick-In Website</a><br />
               <a href="mailto:feedback@kick-in.media">feedback@kick-in.media</a><br />
               <a href="https://github.com/kickin-media">GitHub</a>
