6267fc155bfb1454c6f58c1a49fd712f8153406a
Jeffrey Bakker <jeffrey.bakker@gmail.com>
2022-08-22 17:24:43 +0200

    fix the event preview

--- src/event/components/Event.tsx
+++ src/event/components/Event.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useHistory } from "react-router-dom";
 
 import { EventType } from "../../redux/reducers/event";
@@ -16,19 +16,15 @@ import slugify from "slugify";
 const Event: React.FC<Props> = ({ event }) => {
   const history = useHistory();
 
-  const canViewHidden = useSelector((state: StateType) => state.auth.authenticated
-    && (state.auth.scopes.includes('albums:manage')
-      || state.auth.scopes.includes('photos:upload')
-      || state.auth.scopes.includes('events:manage')));
-
   const albums = useSelector((state: StateType) => event !== null
     ? Object.keys(state.album)
       .filter(key => state.album[key].eventId === event.id)
       .map(key => state.album[key])
-      .filter(album => canViewHidden
-        || (album.photosCount > 0
-          && !(album.releaseTime !== null && new Date(album.releaseTime).getTime() > new Date().getTime())))
-    : [])
+    : []);
+
+  const filteredAlbums = useMemo(() => albums.filter(album => album.photosCount > 0
+    && !(album.releaseTime !== null && new Date(album.releaseTime).getTime() > new Date().getTime())),
+    [albums]);
 
   return event ? (
     <Stack
@@ -42,8 +38,8 @@ const Event: React.FC<Props> = ({ event }) => {
       direction="row"
       spacing={1}
     >
-      {albums.length > 0 && albums[0].coverPhoto
-        ? (<img src={albums[0].coverPhoto?.imgUrls.small} alt="" />)
+      {filteredAlbums.length > 0 && filteredAlbums[0].coverPhoto
+        ? (<img src={filteredAlbums[0].coverPhoto?.imgUrls.small} alt="" />)
         : (<Skeleton variant="rectangular" width={120} height={80} />)}
       <Stack spacing={1}>
         <Typography variant="body1"><strong>{event.name}</strong></Typography>
