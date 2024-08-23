228aaa82a7c2fc9566c72fe772b45e8155c66978
Jeffrey Bakker <jeffrey.bakker@gmail.com>
2024-08-23 14:34:38 +0200

    add confirmation dialogs to some lightbox actions

--- src/app/lightbox/components/lightbox-photo-options/lightbox-photo-options.component.ts
+++ src/app/lightbox/components/lightbox-photo-options/lightbox-photo-options.component.ts
@@ -7,11 +7,16 @@ import { MatDivider } from "@angular/material/divider";
 import { Album, AlbumDetailed, Photo, PhotoDetailed } from "../../../../util/types";
 import { AsyncPipe, NgIf } from "@angular/common";
 import { User } from "@auth0/auth0-angular";
-import { combineLatest, map, Observable, startWith } from "rxjs";
+import { combineLatest, filter, map, Observable, startWith, switchMap } from "rxjs";
 import { AccountService } from "../../../../services/account.service";
 import { toObservable } from "@angular/core/rxjs-interop";
 import { PhotoService } from "../../../../services/api/photo.service";
 import { AlbumService } from "../../../../services/api/album.service";
+import {
+  ConfirmationDialogComponent,
+  ConfirmationDialogProps
+} from "../../../components/confirmation-dialog/confirmation-dialog.component";
+import { MatDialog } from "@angular/material/dialog";
 
 @Component({
   selector: 'lightbox-photo-options',
@@ -40,6 +45,7 @@ export class LightboxPhotoOptionsComponent implements OnChanges {
   protected canDelete$: Observable<boolean>;
 
   constructor(
+    protected dialog: MatDialog,
     protected accountService: AccountService,
     protected albumService: AlbumService,
     protected photoService: PhotoService,
@@ -69,21 +75,63 @@ export class LightboxPhotoOptionsComponent implements OnChanges {
 
   removeFromAlbum() {
     if (!this.closeViewer) return;
-    if (!this.photo || !(this.photo as any).albums) return;
+
+    const photo = this.photo;
+    if (!photo || !(photo as any).albums) return;
 
     const currentAlbum = this.album;
     if (!currentAlbum) return;
 
-    let albums: Album[] = (this.photo as any).albums;
+    let albums: Album[] = (photo as any).albums;
     albums = albums.filter(album => album.id !== currentAlbum.id);
-    this.photoService.setPhotoAlbums(this.photo.id, albums.map(album => album.id));
+
+    const dialogRef = this.dialog.open(
+      ConfirmationDialogComponent,
+      {
+        data: {
+          title: "Are you sure you want to remove this photo from the album?",
+          buttonClass: "error-button",
+          buttonNames: ["CANCEL", "DELETE"],
+        } as ConfirmationDialogProps
+      },
+    );
+
+    (dialogRef.afterClosed() as Observable<boolean>).pipe(
+      filter(result => result),
+      switchMap(() => {
+        return this.photoService.setPhotoAlbums(
+          photo.id,
+          albums.map(album => album.id)
+        );
+      }),
+    ).subscribe(() => {
+      this.albumService.album.refresh();
+      this.closeViewer();
+    });
   }
 
   deletePhoto() {
     if (!this.closeViewer) return;
-    if (!this.photo) return;
 
-    this.photoService.delete(this.photo.id).subscribe(() => {
+    const photo = this.photo;
+    if (!photo) return;
+
+    const dialogRef = this.dialog.open(
+      ConfirmationDialogComponent,
+      {
+        data: {
+          title: "Are you sure you want to permanently delete this photo?",
+          detail: "This will also remove this photo from any other albums it is in.",
+          buttonClass: "error-button",
+          buttonNames: ["CANCEL", "DELETE"],
+        } as ConfirmationDialogProps
+      },
+    );
+
+    (dialogRef.afterClosed() as Observable<boolean>).pipe(
+      filter(result => result),
+      switchMap(() => this.photoService.delete(photo.id)),
+    ).subscribe(() => {
       this.albumService.album.refresh();
       this.closeViewer();
     });
@@ -91,9 +139,10 @@ export class LightboxPhotoOptionsComponent implements OnChanges {
 
   get isInMultipleAlbums(): boolean | null {
     if (!this.album) return null;
-    if (!("albums" in this.album)) return null;
+    if (!this.photo) return null;
+    if (!("albums" in this.photo)) return null;
 
-    const albums: Album[] = (this.album as any).albums;
+    const albums: Album[] = (this.photo as any).albums;
     return albums.length > 0;
   }
 
